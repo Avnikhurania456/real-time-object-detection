682dc6afefd3c215df3f66b3460240fdaa6db622
Avnikhurania456 <111135882+Avnikhurania456@users.noreply.github.com>
2022-08-30 19:29:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -45,6 +45,22 @@ function gotResult(error,results)
 function draw()
 {
     image(video,0,0,480,380);
+    if(status!='')
+    {
+        objectDetector.detect(video.gotResult);
+        for(i=0;i<objects.lenght;i++)   ;
+        {
+            document.getElementById("status").innerHMTL="status:objectsDetected";
+            document.getElementById("number_of_objects").innerHTML="Number of objects detected are"+objects.lenght;
+            fill("#eb348c");
+            percent=floor(object[i].confidence*100);
+            Text(objects[i].lable+""+percent+"%",objects[i].x+15,objects[i].y+15);
+            nofill()
+            stroke("#34ebae");
+            Rect(objects[i].x,objects[i].y,object5[i].width,objects[i].height);
+
+        }          
+    }
 }
 
    
